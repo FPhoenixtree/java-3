f66a45e05fc19cf27f59ddbbdb7a30c7a6dbbe64
FPhoenixtree <309890974@qq.com>
2021-01-08 10:24:26 +0800

    提交

--- src/project3/T34.java
+++ src/project3/T34.java
@@ -1,7 +1,16 @@
 package project3;
+import java.util.Arrays;
 import java.util.Scanner;
 public class T34 {
+	public static String sort(String s) {
+		char[] arr=s.toCharArray();
+		Arrays.sort(arr);
+		String sor = new String(arr);
+		return sor;
+	}
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
+		System.out.println(sort(s));
 	}
 }
--- src/project3/T5.java
+++ src/project3/T5.java
@@ -1,10 +1,25 @@
 package project3;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class T5 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int[] arr = new int[10];
+		Arrays.fill(arr, -1);
+		int len = 0;
+		for(int i = 0;i<10;i++) {
+			int mid = in.nextInt();
+			boolean used = false;
+			for(int j = 0;j<len;j++) {
+				if(arr[j]==mid)used=true;
+			}
+			if(!used) {
+				arr[len++]=mid;
+			}
+		}
+		System.out.println(len);
+		for(int i=0;i<len;i++) {
+			System.out.print(arr[i]+" ");
+		}
 	}
-
 }
